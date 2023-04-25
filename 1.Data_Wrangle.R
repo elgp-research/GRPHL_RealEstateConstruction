506235396db5d451b279bfd83abc7a3c02e3c83a
Haseeb Bajwa <haseeb.sher.bajwa@gmail.com>
2023-04-25 11:33:19 -0400

    Data Import and Wrangle

--- 1.Data_Wrangle.R
+++ 1.Data_Wrangle.R
@@ -2,6 +2,10 @@
 library(tidyverse)
 library(tidycensus)
 library(readxl)
+options(scipen=999)
+
+# Looking at the codebook 
+varlist19 <- load_variables(year = 2019, dataset = "acs5")
 
 ##--1a. CES data import-----------------------------------------------------------------
 
@@ -45,3 +49,53 @@ ces_pctchange <- cleanCESdata[[2]]
 
 abs_data <- read_csv("Data/abs_data.csv")
 abs_data <- abs_data[-1,]
+
+##--3a. IPUMS data----------------------------------------------------------------------
+
+
+##--4a. ACS data------------------------------------------------------------------------
+
+# Looking at the codebook 
+varlist21 <- load_variables(year = 2021, dataset = "acs1")
+
+# Construction Workers variable list from ACS
+varlist <- c(total_employees = "B24060_031", # Total Construction Workers
+  
+             total_male = B24010_117, # Male 
+             total_female = B24010_268, # Female 
+             
+             fulltime_male = B24020_117, # Full time Male
+             fulltime_female = B24020_268, # Full time Female
+             
+             
+             B24010A_032, # White Male 
+             B24010A_068, # White Female 
+             B24010B_032, # Black Male 
+             B24010B_068, # Black Female 
+             B24010C_032, # Native Male
+             B24010C_068, # Native Female
+             B24010D_032, # Asian Male
+             B24010D_068, # Asian Female
+             B24010E_032, # Hawaii Male
+             B24010E_068, # Hawaii Female
+             B24010F_032, # Other race Male
+             B24010F_068, # Other race Female
+             B24010G_032, # 2 or more races Male
+             B24010G_068, # 2 or more races Female
+             B24010H_032, # White alone, Not Hispanic Male
+             B24010H_068, # White alone, Not Hispanic Female
+             B24010I_032, # Hispanic Male
+             B24010I_068 # Hispanic Female
+             )
+
+# setting years 
+years <- lst(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 
+             2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021)
+
+
+
+
+
+
+
+
