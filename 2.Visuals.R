14104f9e8dad276cad8e6ec21aef167ca9606094
Haseeb Bajwa <haseeb.sher.bajwa@gmail.com>
2023-05-03 15:21:51 -0400

    Visuals

--- 2.Visuals.R
+++ 2.Visuals.R
@@ -522,6 +522,53 @@ emp_map_figure
 
 
 
+##--3e. IPUMS Data: Employer Distribution by Age--------------------------------
+
+# Generate color palette
+my_colors <- c("#FF9200", "#1097FF", "#FF4900", 
+               "#7915FF", "#FFBF00")  
+
+# bar graph of ethnic proportions of employers over time 
+ipums_age_fig <- ipums_age %>% 
+  plot_ly(
+    type = 'bar',
+    x =~ YEAR, 
+    y =~ age_prop,
+    color =~ age_bracket,
+    colors = my_colors,
+    text =~ age_bracket,
+    hovertemplate=paste("<i>%{text} in %{x}:</i><br>%{y:.1f}%")
+  ) %>% 
+  layout(barmode = 'stack',
+         title = list(text="<br>      Employer Trends in the Construction Industry<br>      in Greater Philadelphia by Age",
+                      x=0,y=1),
+         font = list(family = "Georgia", color = "darkslategrey"),
+         hoverlabel = list(font = list(family = "Georgia")),
+         yaxis = list(title = "Proportion of Employers (%)"),
+         xaxis = list(title = ""),
+         legend = list(
+           orientation = "h",
+           xanchor = "center",
+           x = 0.5,
+           yanchor = "top",
+           y = -0.1
+         ),
+         annotations = list(
+           x = 1.05, # X position of the caption (right side of the plot)
+           y = 1.1, # Y position of the caption (top of the plot)
+           text = "Source: IPUMS USA Data Estimates", # The text of the caption
+           showarrow = FALSE, # Don't show an arrow pointing to the caption
+           xref = "paper", # Set the X position reference to the plot area
+           yref = "paper", # Set the Y position reference to the plot area
+           font = list(size = 9, color = "grey80"), # Set the font size of the caption
+           align = "right", # Align the caption to the right
+           xanchor = "right", # Anchor the caption to the right side of the plot
+           yanchor = "top" # Anchor the caption to the top of the plot
+         ),
+         margin = list(l = 70, r = 70, b = 50, t = 70)
+  )
+
+ipums_age_fig
 ##--4a. OES Data: Construction Wage Trends----------------------------------------
 
 # filtering for "Manufacturing" and "All Occupations" 
